3048774d15fe45fec5b6e5fa816ce844cc2690b2
Nicolas-Fabian-Sanchez <nicosanchez10@hotmail.com>
2023-05-05 22:45:35 -0300

    cambie index ,contacto y hice menu hambrguesa

--- script/script.js
+++ script/script.js
@@ -2,7 +2,7 @@
 let menuhamburguesa = document.querySelector(".hamburguer");
 let listaDesplegable = document.getElementById("lista");
 
-menuhamburguesa.addEventListener("click",() => {
+menuhamburguesa.addEventListener("click",(event) => {
     console.log("me hicieorn click");
     console.log(listaDesplegable);
     listaDesplegable.classList.toggle("mostrar")
