20d4cf5131ddc1119c187915fe8d354b2e3558a7
Nicolas-Fabian-Sanchez <nicosanchez10@hotmail.com>
2023-05-10 13:12:32 -0300

    nuevo cambio

--- script/inputValidar.js
+++ script/inputValidar.js
@@ -1,14 +1,24 @@
 /* ------- validaciones formulario ------*/
 
 const inputTelefono = document.getElementById("phoneNumber");
-const mensajeError = document.getElementById("mensaje")
+const mensajeError = document.getElementById("mensaje2");
 
 inputTelefono.addEventListener("blur",(event) => {
-        validacionNumero=(event.target.value)
+        let validacionNumero=(event.target.value);
         let numeroIngresado= validacionNumero.toString().length
         if (numeroIngresado != 11){
             mensajeError.classList.add("mostrar")
         }else{
             mensajeError.classList.remove("mostrar")
         }
+})
+
+const inputNombre = document.querySelector(".inputNombre");
+const mensajeError1 = document.getElementById("mensaje1")
+inputNombre.addEventListener("blur",(event)=>{
+     let validacionNombre = (event.target.value);
+     console.log(validacionNombre);
+     let verificar =/^[^A-Z]+$/g
+     let textoVerificado = verificar.test(validacionNombre)
+     console.log(textoVerificado)
 })
